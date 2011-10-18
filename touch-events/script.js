af562a01e7bea361780b6ac658254549d95cfe0b
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-10-18 13:56:50 -0400

    tracking all fingers

--- touch-events/script.js
+++ touch-events/script.js
@@ -1,44 +1,9 @@
 window.onload = function() {
   var obj = document.getElementById('canvas-touch1');
-  //make_circle(obj, "red");
-  //obj.style.background = "red";
-  //obj.style.position = 'relative';
-  //  move(canvas);
-  obj.addEventListener('touchstart', function(event) {
-    if (event.targetTouches.length == 1) {
-      var touch = event.targetTouches[0];
-      var px = touch.pageX;
-      var py = touch.pageY;
-      ctx.beginPath();
-      ctx.arc(px, py, 20,0, 2*Math.PI, true);
-      ctx.fillStyle = "rgba(0, 0, 200, 0.2)";
-      ctx.fill();
-      ctx.lineWidth = 2.0;
-      ctx.strokeStyle = "rgba(0, 0, 200, 0.8)";
-      ctx.stroke();
-  });
-
-  //obj.addEventListener('touchmove', function(event) {
-  //  // If there's exactly one finger inside this element
-  //  if (event.targetTouches.length == 1) {
-  //    var touch = event.targetTouches[0];
-  //    // Place element where the finger is
-  //    obj.style.left = touch.pageX + 'px';
-  //    obj.style.top = touch.pageY + 'px';
-  //  }
-  //}, false);
-  //var canvas = document.getElementById('canvas-touch1');
-  //canvas.width = "300";
-  //canvas.height = "300";
-  //canvas.addEventListener('touchmove', function(event) {
-  //  for (var i=0; i< event.touches.length; i++) {
-  //    var touch = event.touches[i];
-  //    ctx.beginPath();
-  //    ctx.arc(touch.pageX, touch.pageY, 20, 0, 2*Math.PI, true);
-  //    ctx.fill();
-  //    ctx.stroke();
-  //  }
-  //},false);
+  make_circle(obj, "red");
+  obj.style.background = "red";
+  obj.style.position = 'relative';
+  move(canvas);
 }
 
 function make_circle(canvas, color) {
